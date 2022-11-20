32bbfc4da595d1d012874e8f92886e9010f7263a
Paul Vallas <35564306+PaulReaver@users.noreply.github.com>
2022-11-20 13:07:37 +0200

    Populated board with signs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 //Game board module
 const gameBoardModule = (function () {
-    let gameBoard = [];
-    return {};
+    let gameBoard = ["X", "O", "X", "O", "X", "O", "X", "O", "X"];
+    return { gameBoard };
 })();
 
 //Display controller module
@@ -15,6 +15,18 @@ function playerFactory(name, sign) {
     return { name, sign }
 }
 
+//Module to display signs to screen
+const signsToScreen = (function () {
+    const cells = document.querySelectorAll(".cell");
+    for (let i = 0; i < 9; i++) {
+        cells[i].textContent = gameBoardModule.gameBoard[i];
+    }
+    return {};
+})
+
 //Created players
 const playerOne = playerFactory("Paul", "X");
 const playerTwo = playerFactory("Marilena", "O");
+
+//Populate screen with signs
+signsToScreen();
