4e9ec22f872c3adb8eea9fee3077fae068a56ae2
Paul Vallas <35564306+PaulReaver@users.noreply.github.com>
2022-11-23 13:59:02 +0200

    Arrows now show whose player turn is it

--- script.js
+++ script.js
@@ -10,13 +10,14 @@ const displayControllerModule = (function () {
     //Create node list of cells
     const cells = document.querySelectorAll(".cell");
 
-    //Get the players' container
-    const playerOneContainer = document.querySelector("#player-one-container");
-    const playerTwoContainer = document.querySelector("#player-two-container");
+    //Get arrows
+    const rightArrow = document.querySelector("#right-arrow");
+    const leftArrow = document.querySelector("#left-arrow");
 
-    //Set first turn to be X's and change opacity of inactive player
+    //Set first turn to be X's and make inactive player's arrow invisible
     let xTurn = true;
-    playerTwoContainer.style.opacity = "40%";
+    leftArrow.style.opacity = "0";
+    
 
     //Place correct markers on board
     for (let i = 0; i < gameBoardModule.gameBoard.length; i++) {
@@ -24,13 +25,13 @@ const displayControllerModule = (function () {
             if (xTurn) {
                 gameBoardModule.gameBoard[i] = "X";
                 cells[i].textContent = "X";
-                playerOneContainer.style.opacity = "40%";
-                playerTwoContainer.style.opacity = "100%";
+                leftArrow.style.opacity = "1";
+                rightArrow.style.opacity = "0";
             } else {
                 gameBoardModule.gameBoard[i] = "O";
                 cells[i].textContent = "O";
-                playerTwoContainer.style.opacity = "40%";
-                playerOneContainer.style.opacity = "100%";
+                leftArrow.style.opacity = "0";
+                rightArrow.style.opacity = "1";
             }
             xTurn = !xTurn;
         }, { once: true })
