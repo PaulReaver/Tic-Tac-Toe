eb145f2b838055c78874864b630d43ea01762e01
Paul Vallas <35564306+PaulReaver@users.noreply.github.com>
2022-11-23 15:19:30 +0200

    Game now checks for winner and draw

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 //Game board module
 const gameBoardModule = (function () {
+    const winConditions = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
     let gameBoard = ["", "", "", "", "", "", "", "", ""];
-    return { gameBoard };
+    let xTurn = true;
+    return { gameBoard, winConditions, xTurn };
 })();
 
 //Display controller module
@@ -15,14 +26,16 @@ const displayControllerModule = (function () {
     const leftArrow = document.querySelector("#left-arrow");
 
     //Set first turn to be X's and make inactive player's arrow invisible
-    let xTurn = true;
     leftArrow.style.opacity = "0";
-    
+
+
 
     //Place correct markers on board
     for (let i = 0; i < gameBoardModule.gameBoard.length; i++) {
         cells[i].addEventListener("click", () => {
-            if (xTurn) {
+
+
+            if (gameBoardModule.xTurn) {
                 gameBoardModule.gameBoard[i] = "X";
                 cells[i].textContent = "X";
                 leftArrow.style.opacity = "1";
@@ -33,11 +46,45 @@ const displayControllerModule = (function () {
                 leftArrow.style.opacity = "0";
                 rightArrow.style.opacity = "1";
             }
-            xTurn = !xTurn;
+
+
+            //Calls function to check if there is a winner
+            checkWinner();
         }, { once: true })
     }
     return {};
 })();
+
+//function that checks if there is a winner
+function checkWinner() {
+    let roundWon = false;
+
+    for (let i = 0; i < gameBoardModule.winConditions.length; i++) {
+        const condition = gameBoardModule.winConditions[i];
+        const cellA = gameBoardModule.gameBoard[condition[0]];
+        const cellB = gameBoardModule.gameBoard[condition[1]];
+        const cellC = gameBoardModule.gameBoard[condition[2]];
+
+        if (cellA == "" || cellB == "" || cellC == "") {
+            continue;
+        }
+
+        if (cellA == cellB && cellB == cellC) {
+            roundWon = true;
+            break;
+        }
+    }
+
+    if (roundWon) {
+        console.log("round won");
+
+    } else if (!gameBoardModule.gameBoard.includes("")) {
+        console.log("draw")
+
+    } else {
+        gameBoardModule.xTurn = !gameBoardModule.xTurn;
+    }
+}
 
 //Factory function to create players
 function playerFactory(name) {
