e587187502a30ec364e5b049af1b68004fd7c603
Paul Vallas <35564306+PaulReaver@users.noreply.github.com>
2022-12-20 01:20:35 +0200

    Formatted code

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-//Game board module
-const gameBoardModule = (function () {
+// Game board module
+const gameBoardModule = (() => {
     const winConditions = [
         [0, 1, 2],
         [3, 4, 5],
@@ -8,58 +8,36 @@ const gameBoardModule = (function () {
         [1, 4, 7],
         [2, 5, 8],
         [0, 4, 8],
-        [2, 4, 6]
+        [2, 4, 6],
     ];
-    let gameBoard = ["", "", "", "", "", "", "", "", ""];
-    let xTurn = true;
-    let running = true;
+    const gameBoard = ['', '', '', '', '', '', '', '', ''];
+    const xTurn = true;
+    const running = true;
     return { gameBoard, winConditions, xTurn, running };
 })();
 
-//Display controller module
-const displayControllerModule = (function () {
+// Factory function to create players
+function playerFactory(name) {
+    return { name };
+}
 
-    //Create node list of cells
-    const cells = document.querySelectorAll(".cell");
-
-    //Get display message
-    const message = document.querySelector(".message");
-
-    //Place correct markers on board
-    for (let i = 0; i < gameBoardModule.gameBoard.length; i++) {
-        cells[i].addEventListener("click", () => {
-            if (gameBoardModule.xTurn && gameBoardModule.running) {
-                gameBoardModule.gameBoard[i] = "X";
-                cells[i].textContent = "X";
-                message.textContent = "player O's turn..."
-            } else if (!gameBoardModule.xTurn && gameBoardModule.running) {
-                gameBoardModule.gameBoard[i] = "O";
-                cells[i].textContent = "O";
-                message.textContent = "player X's turn..."
-            }
-
-            //Calls function to check if there is a winner
-            if (gameBoardModule.running) {
-                checkWinner(message);
-            }
-        }, { once: true })
-    }
-    return {};
-})();
-
-//function that checks if there is a winner
+// function that checks if there is a winner
 function checkWinner(message) {
     let roundWon = false;
 
-    for (let i = 0; i < gameBoardModule.winConditions.length; i++) {
+    const currentMessage = message;
+
+    for (let i = 0; i < gameBoardModule.winConditions.length; i += 1) {
         const condition = gameBoardModule.winConditions[i];
         const cellA = gameBoardModule.gameBoard[condition[0]];
         const cellB = gameBoardModule.gameBoard[condition[1]];
         const cellC = gameBoardModule.gameBoard[condition[2]];
-        if (cellA == "" || cellB == "" || cellC == "") {
-            continue;
-        }
-        if (cellA == cellB && cellB == cellC) {
+
+        if (
+            cellA === cellB &&
+            cellB === cellC &&
+            !(cellA === '' || cellB === '' || cellC === '')
+        ) {
             roundWon = true;
             break;
         }
@@ -69,23 +47,54 @@ function checkWinner(message) {
         gameBoardModule.running = false;
 
         if (gameBoardModule.xTurn) {
-            const playerOne = playerFactory(document.querySelector("#player-one").value);
-            message.textContent = `the winner is ${playerOne.name}!`
+            const playerOne = playerFactory(
+                document.querySelector('#player-one').value
+            );
+            currentMessage.textContent = `the winner is ${playerOne.name}!`;
         } else {
-            const playerTwo = playerFactory(document.querySelector("#player-two").value);
-            message.textContent = `the winner is ${playerTwo.name}!`
+            const playerTwo = playerFactory(
+                document.querySelector('#player-two').value
+            );
+            currentMessage.textContent = `the winner is ${playerTwo.name}!`;
         }
-
-    } else if (!gameBoardModule.gameBoard.includes("")) {
-        message.textContent = `draw!`;
+    } else if (!gameBoardModule.gameBoard.includes('')) {
+        currentMessage.textContent = `draw!`;
         gameBoardModule.running = false;
     } else {
         gameBoardModule.xTurn = !gameBoardModule.xTurn;
     }
 }
 
-//Factory function to create players
-function playerFactory(name) {
-    return { name }
-}
+// Display controller module
+(() => {
+    // Create node list of cells
+    const cells = document.querySelectorAll('.cell');
 
+    // Get display message
+    const message = document.querySelector('.message');
+
+    // Place correct markers on board
+    for (let i = 0; i < gameBoardModule.gameBoard.length; i += 1) {
+        cells[i].addEventListener(
+            'click',
+            () => {
+                if (gameBoardModule.xTurn && gameBoardModule.running) {
+                    gameBoardModule.gameBoard[i] = 'X';
+                    cells[i].textContent = 'X';
+                    message.textContent = "player O's turn...";
+                } else if (!gameBoardModule.xTurn && gameBoardModule.running) {
+                    gameBoardModule.gameBoard[i] = 'O';
+                    cells[i].textContent = 'O';
+                    message.textContent = "player X's turn...";
+                }
+
+                // Calls function to check if there is a winner
+                if (gameBoardModule.running) {
+                    checkWinner(message);
+                }
+            },
+            { once: true }
+        );
+    }
+    return {};
+})();
